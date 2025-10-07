0dfda61cbbd6e6fe84bf86fafe1207afee4e4287
Abhinavdotcom <abhinav.kunjir@mitwpu.edu.in>
2025-10-07 17:38:00 +0530

    Add files via upload

---
+++ datatype.c
@@ -0,0 +1,20 @@
+/*
+
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    char cValue = 'S';
+    int iValue = 11;
+    float fValue = 90.78f;
+    double dValue = 90.73782;
+
+    printf("%c\n ",cValue);
+    printf("%d\n ",iValue);
+    printf("%f\n ",fValue);
+    printf("%f\n ",dValue);
+
+    return 0;
+}
